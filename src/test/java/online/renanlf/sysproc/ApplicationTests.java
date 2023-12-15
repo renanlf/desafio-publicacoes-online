e96aa4ed3bd1bc407b7d22aff99c4c04fd99e98a
Renan Leandro Fernandes <leandrorenanf@gmail.com>
2023-12-15 20:37:25 -0300

    commit inicial do projeto

---
+++ src/test/java/online/renanlf/sysproc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package online.renanlf.sysproc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
