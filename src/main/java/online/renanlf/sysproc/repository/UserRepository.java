943ca5206520d6f9b825f36646bc396e3fc8dc35
Renan Leandro Fernandes <leandrorenanf@gmail.com>
2023-12-17 17:51:03 -0300

    atualizando repositório dos usuários

--- src/main/java/online/renanlf/sysproc/repository/UserRepository.java
+++ src/main/java/online/renanlf/sysproc/repository/UserRepository.java
@@ -3,20 +3,15 @@ package online.renanlf.sysproc.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import online.renanlf.sysproc.model.User;
 
 public interface UserRepository  extends JpaRepository<User, Long> {
 	
-	@Query(value = "SELECT u FROM User u WHERE u.token = :token")
-	Optional<User> findByToken(@Param("token") String token);
+	Optional<User> findByToken(String token);
 
-	@Query(value = "SELECT u FROM User u WHERE u.email = :email AND u.password = :password")
-	Optional<User> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+	Optional<User> findByEmailAndPassword(String email, String password);
 	
-	@Query(value = "SELECT u FROM User u WHERE u.email = :email")
-	Optional<User> findByEmail(@Param("email") String email);
+	Optional<User> findByEmail(String email);
 	
 }
